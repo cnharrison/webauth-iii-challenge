c869bb3f53ff6a26251fcc3e28df2bcfd5d6bbdf
Christopher Harrison <cnharrison@gmail.com>
2019-06-13 22:52:15 -0400

    middlewares and auth router post

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-const express = require("express");
-const knex = require("knex");
-const knexConfig = require("./knexfile.js");
+require("dotenv").config();
 
-const server = express();
-const db = knex(knexConfig);
+const server = require("./api/server.js");
 
-server.use(express.json());
-server.use(helmet());
-
-const port = 3300;
-server.listen(port, function() {
-  console.log(`\nusers api 👨‍🔬👨‍🔧👨‍🍳👩‍🌾 http://localhost:${port} ===\n`);
-});
+const port = process.env.PORT || 5000;
+server.listen(port, () =>
+  console.log(`\n** users api 👨‍🔬👨‍🔧👨‍🍳👩‍🌾 on port ${port} **\n`)
+);
